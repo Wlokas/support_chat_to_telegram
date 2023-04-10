3bd5b9c7b5e5dc46fbcd1d324dc6bbca9a2f8144
Maxim <43240747+Wlokas@users.noreply.github.com>
2023-04-10 20:32:09 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,19 +1,19 @@
 <?php
     define('DB', [
         'host' => 'localhost',
-        'user' => 'wlokas',
-        'pass' => '7s382s33',
+        'user' => '',
+        'pass' => '',
         'base' => 'chat'
     ]);
 
     define('CFG', [
 
-        'token' => '1429677038:AAH2eWhhlLunkUEJuL6O36yHhpoAfX3_Yk0',
-        'chat_id' => '-283386324',
+        'token' => '',
+        'chat_id' => '',
         'timeout' => 300,
 
         'proxy' => [
-            'host' => 'kate.onlinesim.ru:17233',
-            'auth' => 'gf9g9k1r:3wh28b1a'
+            'host' => '',
+            'auth' => 'a'
         ],
     ]);
